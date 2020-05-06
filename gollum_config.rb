c5770a1aca6ace197295b246b7450de55d528e53
Derek Kozel <derek@bitstovolts.com>
2020-05-06 14:56:34 +0100

    gollum: Updated post commit hook with credentials

--- gollum_config.rb
+++ gollum_config.rb
@@ -1,4 +1,5 @@
 Gollum::Hook.register(:post_commit, :hook_id) do |committer, sha1|
-     committer.wiki.repo.git.pull('origin', committer.wiki.ref)
-     committer.wiki.repo.git.push('origin', committer.wiki.ref)
+     credentials = Rugged::Credentials::SshKeyFromAgent.new(username: 'git')
+     committer.wiki.repo.git.pull('origin', committer.wiki.ref, credentials: credentials)
+     committer.wiki.repo.git.push('origin', committer.wiki.ref, credentials: credentials)
 end
